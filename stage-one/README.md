d0972745766f04c8ead508ef705c6875b0c121e2
mukadas026 <mukadasm026@gmail.com>
2023-09-07 16:53:32 +0000

    add javascript code

---
+++ stage-one/README.md
@@ -0,0 +1,3 @@
+# Stage One
+
+<a href="#">Click here to see site</a>
--- stage-one/script.js
+++ stage-one/script.js
@@ -4,8 +4,24 @@ const timeEl = document.querySelector('.time')
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 
-const date = new Date()
 
+let date = new Date()
 dayEl.innerHTML = daysOfWeek[date.getDay()]
 
-console.log(date.getUTCMilliseconds())
+setInterval(() => {
+    date = new Date()
+let minute = date.getUTCMinutes()
+let hr = date.getUTCHours()
+let sec = date.getUTCSeconds()
+let mil = date.getUTCMilliseconds()
+
+// format all of them 
+hr = hr >= 10 ? `${hr}` : `0${hr}`
+minute = minute >= 10 ? `${minute}` : `0${minute}`
+sec = sec >= 10 ? `${sec}` : `0${sec}`
+mil = mil < 10 ? `00${mil}` : mil < 100 ? `0${mil}` : `${mil}`
+
+timeEl.innerHTML = `${hr}:${minute}:${sec}:${mil}`
+
+// console.log(hr, minute, sec, mil)
+}, 1)
